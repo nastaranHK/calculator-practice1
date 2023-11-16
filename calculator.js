4223544cf1e06be446b768e14aedd07edec58a30
nastaranHK <nastaranhoseinikaram@gmail.com>
2023-11-17 00:35:25 +0330

    made with html css and commited

---
+++ calculator.js
@@ -0,0 +1,8 @@
+const userInput = document.getElementById('calcinput');
+const add = document.getElementById('add-btn');
+const subtract = document.getElementById('subtract-btn');
+const multiply = document.getElementById('multiply-btn');
+const devide = document.getElementById('divide-btn');
+const result = document.getElementById('result');
+
+
